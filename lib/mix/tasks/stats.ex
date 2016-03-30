f5e21cc25d6ee44cbe48acb622a345c56236ba9d
Stuart Hunt <onno.owl@gmail.com>
2016-03-30 17:56:08 -0400

    Fixed up stats.ex module.

--- lib/mix/tasks/stats.ex
+++ lib/mix/tasks/stats.ex
@@ -1,8 +1,6 @@
 defmodule Mix.Tasks.Stats do
   use Mix.Task
 
-  def default_args(), do: %{input_ids: [:x, :y], output_ids: [:a, :b, :c, :d], memory_size: 3}
-
   def get_data(module, learn_val, args) do
     to_train = module.new(args)
     role_model = module.new(args)
@@ -24,12 +22,12 @@ defmodule Mix.Tasks.Stats do
     IO.puts "#{str} | Mean: #{Float.round(mean, digits)}, SD: #{Float.round(sd, digits)}, N: #{n}"
   end
 
-  def compare(module1, module2, args1 \\ default_args(), args2 \\ default_args(), sample_size \\ 40) do
+  def compare(module1, module2, argfun1, argfun2, sample_size \\ 200) do
     {time_data, its_data} = Enum.reduce 1..sample_size, {[], []}, fn num, {time_data, its_data} ->
       learn_val = 0.5 + :random.uniform*2.5
-      IO.write "\rCollecting data sample #{num}/#{sample_size} with learn_val #{learn_val}"
-      {time1, iterations1} = get_data(module1, learn_val, args1)
-      {time2, iterations2} = get_data(module2, learn_val, args2)
+      IO.write "\rCollecting data sample #{num}/#{sample_size} with learn_val #{Float.round(learn_val, 3)}"
+      {time1, iterations1} = get_data(module1, learn_val, argfun1.())
+      {time2, iterations2} = get_data(module2, learn_val, argfun2.())
       {[time1 - time2 | time_data], [iterations1 - iterations2 | its_data]}
     end
     IO.puts ""
@@ -38,6 +36,12 @@ defmodule Mix.Tasks.Stats do
   end
 
   def run(_) do
-    compare(GRU, GRUM)
+    argfun = fn ->
+      in_size = :random.uniform(10)
+      out_size = :random.uniform(10)
+      %{input_ids: Enum.to_list(1..in_size), output_ids: Enum.to_list(1..out_size), memory_size: out_size}
+    end
+
+    compare(GRU, GRUM, argfun, argfun)
   end
 end
